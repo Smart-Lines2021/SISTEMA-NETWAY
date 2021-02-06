2e2e5382d27a1937396ed2e2eeb0bedd089c0d7b
JuanAngelMolinaDT <juanangelmolinadt2018@gmail.com>
2021-02-06 14:03:17 -0600

    Añadi modelos y migraciones (sin configurar aun)

---
+++ database/migrations/2021_02_06_195349_create_categorias_proveedores_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriasProveedoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias_proveedores', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorias_proveedores');
+    }
+}
