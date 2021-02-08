104858a573208084713a2172d50c58d63081cf7b
jesusrv1103 <jesus21c@hotmail.com>
2021-02-07 22:35:37 -0600

    Valor del Dolar

--- resources/views/admin/layout/home.blade.php
+++ resources/views/admin/layout/home.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layout.layout')
 @section('title')
-   <h1 class="m-0 text-dark">Inicio</h1>
+<h1 class="m-0 text-dark">Inicio</h1>
 @endsection
 @section('content-header')
 <ol class="breadcrumb float-sm-right">
@@ -10,20 +10,78 @@
 @stop
 
 @section('content')
+
+<div class="row">
+    <div class="col-md-3 col-sm-6 col-12">
+        <div class="info-box">
+            <span class="info-box-icon bg-warning"><i class="far fa-money-bill-alt"></i></span>
+
+            <div class="info-box-content ">
+                <span class="info-box-text">1 dólar estadounidense Es igual a</span>
+            <span class="info-box-number">{{$precioDelDolar}}</span> <span class="info-box-text"> peso mexicano</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-3 col-sm-6 col-12">
+        <div class="info-box">
+            <span class="info-box-icon bg-success"><i class="far fa-flag"></i></span>
+
+            <div class="info-box-content">
+                <span class="info-box-text">Bookmarks</span>
+                <span class="info-box-number">410</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-3 col-sm-6 col-12">
+        <div class="info-box">
+            <span class="info-box-icon bg-warning"><i class="far fa-copy"></i></span>
+
+            <div class="info-box-content">
+                <span class="info-box-text">Uploads</span>
+                <span class="info-box-number">13,648</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-3 col-sm-6 col-12">
+        <div class="info-box">
+            <span class="info-box-icon bg-danger"><i class="far fa-star"></i></span>
+
+            <div class="info-box-content">
+                <span class="info-box-text">Likes</span>
+                <span class="info-box-number">93,139</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+</div>
 <!-- Small boxes (Stat box) -->
 <div class="row">
     <div class="col-lg-3 col-6">
         <!-- small box -->
         <div class="small-box bg-info">
             <div class="inner">
-                <h3>150</h3>
-
-                <p>New Orders</p>
+                <p>1 dólar estadounidense Es igual a
+                    <br>
+                    <strong>{{$precioDelDolar}}</strong><br>
+                    peso mexicano
+                </p>
             </div>
             <div class="icon">
                 <i class="ion ion-bag"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="https://www.google.com/search?q=precio+del+dolar&rlz=1C5CHFA_enMX939MX939&oq=prec&aqs=chrome.0.69i59l2j69i57j69i59j0i433j69i60l2j69i61.2311j0j9&sourceid=chrome&ie=UTF-8"
+                class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
     </div>
     <!-- ./col -->
